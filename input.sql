47d6af5582eb36093a880656a7e6faf1fb0d2083
heavenbeing <heavenbeing@outlook.com>
2019-03-29 15:10:48 +0800

    输入sql示例 sql example

---
+++ input.sql
@@ -0,0 +1,24 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost-mysql
+ Date: 28/03/2019 13:48:52
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for info
+-- ----------------------------
+DROP TABLE IF EXISTS `info`;
+CREATE TABLE `info`  (
+  `id` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '信息表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+INSERT INTO `info` VALUES ('4', 'ww4');
+
